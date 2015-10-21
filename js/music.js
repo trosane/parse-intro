c4ac40a02f69fd9375b6d57ff43b4367a411e398
Trent Rosane <trosane@uw.edu>
2015-10-21 09:58:03 -0700

    final

--- js/music.js
+++ js/music.js
@@ -1,6 +1,5 @@
 // Initialize Parse app
-Parse.initialize("CCsV65azRRrpRQHyuHBXdbjWHFHtsTg3YGXDdcML", "7vftCEgw7ANHT8KxD2g1kWWFFr636NGumX14o1SD");
-
+Parse.initialize("qy2RLKMkO8x2VLNzwejI36e4SLxv4OuzISilPqtA", "1W4XlMhbQoBJePu8IXsdWAhe39x036zoUwEPfXhM")
 // Create a new sub-class of the Parse.Object, with name "Music"
 var Music = Parse.Object.extend("Music");
 
@@ -36,8 +35,9 @@ $('form').submit(function() {
 	});
 
 	// After setting each property, save your new instance back to your database
-	musicItem.save();
-	
+	musicItem.save(null, function() {
+        success:getData
+    });
 	return false
 })
 
@@ -48,38 +48,57 @@ var getData = function() {
 	
 
 	// Set up a new query for our Music class
-
+    var query = new Parse.Query(Music);
 
 	// Set a parameter for your query -- where the website property isn't missing
-
+    query.exists('website');
 
 	/* Execute the query using ".find".  When successful:
 	    - Pass the returned data into your buildList function
 	*/
+    query.find({
+        success:function(response) {
+            buildList(response)
+        }
+    });
+
 }
 
 // A function to build your list
 var buildList = function(data) {
 	// Empty out your unordered list
-	
+	$('ol').empty();
 	// Loop through your data, and pass each element to the addItem function
-
+    data.forEach(function(d) {
+        addItem(d);
+    });
 }
 
 
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
+    var website = item.get("website");
+    var band = item.get("band");
+    var song = item.get("song");
 
 	
 	// Append li that includes text from the data item
-
+    var li = $('<li>' + band + ' ' + website + ' ' + song + '</li>')
+    var button = $('<button class="btn-xs btn-danger"><span class="glyphicon glyphicon-minus"></span></button>')
+    button.on('click', function() {
+        item.destroy({
+            success:getData
+        })
+    })
+    li.append(button);
+    $("#list").append(li);
 
 	
 	// Time pending, create a button that removes the data item on click
-	
+
 }
 
 // Call your getData function when the page loads
-
+getData();
 
